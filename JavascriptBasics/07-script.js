bee2a2161feaa5aee098f7429ab5d33126807853
Chris Davis <chris.davis5440@gmail.com>
2017-06-05 10:17:41 -0400

    JS Basics 7 - Array Quiz

---
+++ JavascriptBasics/07-script.js
@@ -0,0 +1,20 @@
+//Array Quiz
+
+//Problem 1
+var numbers = [22,67,33,96,88];
+
+//What does this print?
+console.log(numbers[numbers.length]); //undefined
+
+
+
+
+//Problem 2
+var friendGroups = [
+	["Samphia", "Dangus", "Prishpreed"],
+	["Krangus", "Krabbit", "Sol"],
+	["Tim", "Steve", "Eric"]
+];
+
+//What is the result of this code?
+console.log(friendGroups[2][0]); //Tim
