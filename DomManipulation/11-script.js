42dbffa7f59c4767cd91be3502b30be4b5420bb1
Chris Davis <chris.davis5440@gmail.com>
2017-06-07 13:49:57 -0400

    Setup initial logic for color comparing

--- DomManipulation/11-script.js
+++ DomManipulation/11-script.js
@@ -1,2 +1,33 @@
 //Dom Manipulation 11 - RGB Color Game
 
+var colors = [
+	"rgb(255, 0, 0)",
+	"rgb(255, 255, 0)",
+	"rgb(0, 255, 0)",
+	"rgb(0, 255, 255)",
+	"rgb(0, 0, 255)",
+	"rgb(255, 0, 255)"
+];
+
+var squares = document.querySelectorAll(".square");
+var pickedColor = colors[3];
+var colorDisplay = document.querySelector("#colorDisplay");
+
+colorDisplay.textContent = pickedColor;
+
+for(var i = 0; i < squares.length; i++) {
+	//Add initial colors to squares
+	squares[i].style.backgroundColor = colors[i];
+
+	//Add click event listeners to squares
+	squares[i].addEventListener("click", function() {
+		//Grab color of clicked square
+		var clickedColor = this.style.backgroundColor;
+		//Compare color to picked color
+		if(clickedColor === pickedColor) {
+			alert("Correct!");
+		}else {
+			alert("Wrong!");
+		}
+	});
+}
