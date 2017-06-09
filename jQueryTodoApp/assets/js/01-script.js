edfef2c56f9391976b09c7ce712b91bef71f0d7a
Chris Davis <chris.davis5440@gmail.com>
2017-06-09 09:44:47 -0400

    Updated click events to handle new appended todos

--- jQueryTodoApp/assets/js/01-script.js
+++ jQueryTodoApp/assets/js/01-script.js
@@ -3,13 +3,13 @@
 $(function() {
 
 	//Check off specific todos by clicking
-	$("li").on("click", function() {
+	$("ul").on("click", "li", function() {
 		$(this).toggleClass("completed");
 	});
 
 	//Click on X to delete specific todo - fadeOut then remove
 	//Stop propagation from bubbling up to the li click event handler
-	$("span").on("click", function(event) {
+	$("ul").on("click", "span", function(event) {
 		event.stopPropagation();
 		$(this).parent().fadeOut(500, function() {
 			$(this).remove();
@@ -21,7 +21,7 @@ $(function() {
 		if(event.which === 13) {
 			//Grab new todo text from input
 			var todoText = $(this).val();
-			//Create new li with todo text and append to ul
+			//Create new li with todo text and append to uls
 			$("ul").append("<li><span>X</span> " + todoText + "</li>");
 			//Clear text within the text input
 			$(this).val("");
