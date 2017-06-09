53564a8688c7171929c1a2b8401dfd1474852d9e
Chris Davis <chris.davis5440@gmail.com>
2017-06-09 11:36:42 -0400

    jQuery Patatap App - Setup Paper JS

---
+++ jQueryPatatap/assets/js/01-script.js
@@ -0,0 +1,22 @@
+//jQuery Todo Projects 02 - jQuery Patatap App
+
+$(function() {
+
+	var canvas = document.getElementById('myCanvas');
+  // Create an empty project and a view for the canvas:
+  paper.setup(canvas);
+
+  var myPath = new paper.Path();
+  myPath.strokeColor = 'red';
+
+  // Draw the view now:
+  paper.view.draw();
+
+  var tool = new paper.Tool();
+
+  tool.onMouseDown = function(event) {
+      myPath.add(event.point);
+  }
+
+  
+});
